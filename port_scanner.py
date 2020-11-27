e563ea7641e8ffc6b8cbf8ec68e35cf64f9431c4
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2020-11-27 20:41:52 +0530

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(5)
@@ -14,12 +15,35 @@ print(
  	'''
 	)
 
-host = input("Enter the IP you want to Scan> ")
-port = int(input("Enter the Port you want to Scan> "))
 
-def portScanner(port):
-	if s.connect_ex((host, port)):
+def portScanner(ip, port):
+	if s.connect_ex((ip, port)):
 		print("The port is closed.")
 	else:
 		print("The port is open.")
-portScanner(port)
+
+def help_msg():
+    print('''
+Usage: python3 port_scanner.py [IP Address to scan] [Port]
+
+Example:
+
+python3 port_scanner.py 172.217.161.4 1337
+
+''')
+    quit()
+
+if "help" in sys.argv:
+    help_msg()
+else:
+    try:
+        print(sys.argv[1])
+        print(sys.argv[2])
+        try:
+            portScanner(sys.argv[1],int(sys.argv[2]))
+        except Exception as e:
+            print('An error occured.')
+            print(e)
+    except:
+        print('WRONG USAGE.')
+        help_msg()
